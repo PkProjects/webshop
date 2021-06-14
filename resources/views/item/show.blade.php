8af7097fa931f2ff056cb18cc6b20fe5753110b1
Ivar Polman <ivarpolman@hotmail.com>
2021-06-14 16:24:35 +0200

    Added 'add to cart button'

--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -19,31 +19,41 @@
             <p>{{ $item->summary }}</p>
 
             @if($item->supply == '1')         
-                <div>&#128994; In stock</div>
+                <div class="mb-3">&#128994; In stock</div>
             @else
-                <div>&#128308; Out of stock</div>
+                <div class="mb-3">&#128308; Out of stock</div>
             @endif
+
+            <button type="button" class="btn btn-success">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                </svg> Add to cart
+            </button>
+
         </div>
     </div>
 
     <div class="row">
         <h2 class="mb-3">Reviews</h2>
+    </div>
 
         @foreach ($item->reviews as $review)
 
-        <div>By {{ $review->user->name }}</div>
-        <div>{{ date("d/m/Y", strtotime($review->created_at)) }}</div>
+        <div class="row mb-2">
 
-        <div>&#10133; {{ $review->pros }}</div>
-        <div class="mb-4">&#10134; {{ $review-> cons }}</div>
+            <div>By {{ $review->user->name }}</div>
+            <div>{{ date("d/m/Y", strtotime($review->created_at)) }}</div>
 
-        <p>{{ $review->review }}</p>
+            <div>&#10133; {{ $review->pros }}</div>
+            <div class="mb-4">&#10134; {{ $review-> cons }}</div>
 
-        <div>Rating: {{ $review->rating}} out of 5</div>
+            <p>{{ $review->review }}</p>
+
+            <div>Rating: {{ $review->rating}} out of 5</div>
+
+        </div>
 
         @endforeach
-
-    </div>
 
 </div>
 
