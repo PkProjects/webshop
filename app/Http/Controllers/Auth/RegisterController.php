ef995c44ad8df9c8a1e78494f8828cea84e0d211
Cas <cvalkenet@yahoo.com>
2021-06-14 15:07:36 +0200

    Register fix + user info page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,8 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'adress' => '1',
+            'phone_number' => '2',
             'password' => Hash::make($data['password']),
         ]);
     }
